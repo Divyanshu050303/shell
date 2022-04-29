7788c1fc1a3836eb27de480d9d061423d3128ecf
Divyanshu050303 <divyanshusingh7060@gmail.com>
2022-04-30 01:25:35 +0530

    FIndTheDay

---
+++ findTheDay.sh
@@ -0,0 +1,20 @@
+# input the day number and print the day name
+echo "Enter the day number"
+read a
+case $a in
+1) echo "Monday"
+    ;;
+2) echo "Tuesday"
+    ;;
+3) echo "Wednesday"
+    ;;
+4) echo "Thrusday"
+    ;;
+5) echo "friday"
+    ;;
+6) echo "Saturday"
+    ;;
+7) echo "Sunday"
+    ;;
+*)echo "Wrong choice"
+esac
