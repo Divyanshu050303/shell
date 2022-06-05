8718085f869eeab931b4100b4912c4c8987c8fdb
divyanshu050303 <divyanshusingh6070@gmail.com>
2022-06-05 20:33:00 +0530

    print the number of line word and character in the file

---
+++ printTheWordLineInTheFile.sh
@@ -0,0 +1,7 @@
+#Print the numner of character and word and number of line in the file
+numberOfChar=$(wc -c <$1)
+echo "Number of character $numberOfChar"
+numberOfWord=$(wc -w < $1)
+echo "Number of word $numberOfWord "
+numberOfLine=$(wc -l < $1 )
+echo "Number of line $numberOfLine"
