3b92b82556a46b8ff081102b445d6c770e9acb01
divyanshu050303 <divyanshusingh6070@gmail.com>
2022-06-05 20:37:21 +0530

    vowel or consonant

---
+++ vowel.sh
@@ -0,0 +1,10 @@
+#find out input character vowel digit or other character
+
+echo "Enter the data"
+read a
+case $a in
+[0-9]) echo "Digit" ;;
+a|e|i|o|u) echo "Small vowel character";;
+A|E|I|O|U) echo "capital letter vowel character";;
+*) echo "Other character"
+esac
