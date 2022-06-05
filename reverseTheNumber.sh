71007be0fc3015ef76eee6366fa9bc2ce8f46b93
divyanshu050303 <divyanshusingh6070@gmail.com>
2022-06-05 20:30:51 +0530

    reverse the number

---
+++ reverseTheNumber.sh
@@ -0,0 +1,17 @@
+#Reverse the number
+echo "Enter the number which you want to reverse"
+read n
+rem=0
+rev=0
+if [ $n -lt 0 ]
+then
+    echo "Number is not positive"
+else
+    while [ $n -gt 0 ]
+    do
+        rem=$(( $n % 10 ))
+        rev=$(( $rev * 10 + $rem ))
+        n=$(( $n / 10 ))
+    done
+    echo "Reverse number of entered digit is $rev"
+fi
