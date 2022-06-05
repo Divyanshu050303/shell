278da4039e0ec749b2b3afcb176bd91746273b83
divyanshu050303 <divyanshusingh6070@gmail.com>
2022-06-05 20:36:00 +0530

    extract

---
+++ extrat.sh
@@ -0,0 +1,10 @@
+#extract the day name from the command date and use it to print the following information
+# monday wednesday friday backup
+# tuesday thrusday partial back up
+# other dat no backup
+c=$(date +%u)
+case $c in
+1|3|5) echo "Full Backup";;
+2|4) echo "Partial backup";;
+*) echo "No backup"
+esac
